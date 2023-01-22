4ce9c250d498ae67ed81451959345d22a8890f5c
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-22 23:19:34 +0530

    Update mass_action.py

--- MassActionBot/plugins/mass_action.py
+++ MassActionBot/plugins/mass_action.py
@@ -1,8 +1,9 @@
-from MassActionBot import app
+from MassActionBot import app,BOT_ID
 from pyrogram import filters
 from MassActionBot.utils.chat_status import handle_status
 
 @app.on_message(filters.command("muteall"))
 @handle_status
 async def ntg(_, message):
+    print(BOT_ID)
     await message.reply_text("yes done")
