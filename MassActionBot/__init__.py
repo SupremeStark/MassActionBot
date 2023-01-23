91dec66be519308c2a33659cfbb8a1c7ce626eef
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-23 07:14:30 +0530

    Update __init__.py

--- MassActionBot/__init__.py
+++ MassActionBot/__init__.py
@@ -31,7 +31,7 @@ app = Client(
     "SupremeStark",
     api_id = API_ID,
     api_hash = API_HASH,
-    bot_token = TOKEN 
+    bot_token = TOKEN ,
     plugins= dict(root="MassActionBot/plugins"))
     
 
