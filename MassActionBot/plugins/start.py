59c3cfced1c617c314f27cc83786f3d3c2608a1c
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-23 07:16:27 +0530

    Update start.py

--- MassActionBot/plugins/start.py
+++ MassActionBot/plugins/start.py
@@ -2,7 +2,5 @@ from MassActionBot import app
 from pyrogram import filters 
 
 @app.on_message(filters.command("start"))
-async def _start(_, message):
-    BOT_ID = (await _.get_me()).id
-    print(BOT_ID)
+async def _start(_, message):    
     await message.reply_text("am started")
