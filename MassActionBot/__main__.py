35f2b8bf72b187ddf4dd522de16f4c50b3c571c7
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-23 07:05:55 +0530

    Update __main__.py

--- MassActionBot/__main__.py
+++ MassActionBot/__main__.py
@@ -1,9 +1,18 @@
 import asyncio
+import importlib 
 from pyrogram import idle 
-
+from MassActionBot.plugins import ALL_MODULE 
 
 
 
 async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("MassActionBot.plugins." + all_module)
     LOG.print("[bold yellow]ɴᴏᴡ ᴀᴍ ʀᴇᴀᴅʏ ᴛᴏ ғɪɢʜᴛ ʙᴏss..")
-    await idle()    
+    await idle() 
+    LOG.print("[bold red]sᴛᴏᴘᴘɪɴɢ ᴛʜᴇ ʙᴏᴛ !")   
+
+
+if __name__ == "__main__" :
+    asyncio.get_event_loop().run_until_complete(start_bot()) 
+    
