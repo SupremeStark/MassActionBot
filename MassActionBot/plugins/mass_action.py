f7b5fccb5de8118a6f98d71d7808c913127d7834
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-23 07:55:29 +0530

    Update mass_action.py

--- MassActionBot/plugins/mass_action.py
+++ MassActionBot/plugins/mass_action.py
@@ -1,7 +1,9 @@
+import asyncio
 from MassActionBot import app,SUDOES
-from pyrogram import filters
+from pyrogram import filters,enums
 from MassActionBot.utils.chat_status import handle_status
 from MassActionBot.plugins.cancel_process import SPAM_CHATS
+from pyrogram.errors import FloodWait 
 
 @app.on_message(filters.command(["banall","unbanall"]))
 @handle_status
@@ -14,6 +16,25 @@ async def _banUnban(_, message):
             if chat_id not in SPAM_CHATS:
                 break
             try:
-               if user_id in SUDOES:
-                   continue 
+                if user_id in SUDOES:
+                    pass
+                try:
+                    await _.ban_chat_member(chat_id,members.user.id)
+                    await _.send_message(chat_id,f"ʙᴀɴɴᴇᴅ {members.user.mention} ɪɴ `{message.chat.title}`.") 
+                except FloodWait as ok:
+                    await asyncio.sleep(ok.value) 
+    if message.command[0] == "unbanall":
+        banned_users = []
+        x = 0
+        async for m in pgram.get_chat_members(chat_id,filter=enums.ChatMembersFilter.BANNED):
+            banned_users.append(m.user.id)
+            if chat_id not in SPAM_CHATS:
+                break       
+            try:               
+                await pgram.unban_chat_member(chat_id,banned_users[x])
+                await _.send_message(chat_id,f"ᴜɴʙᴀɴɪɴɢ ᴀʟʟ ᴍᴄ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ {m.user.mention}")
+                x += 1                                                                
+            except FloodWait as e:
+                await asyncio.sleep(e.value)           
+     #   end = get_readable_time((time.time() - start))      
                 
