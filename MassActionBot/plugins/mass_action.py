d1751a75d8f56bd72aea7273caced9f7a1ffe9f5
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-23 07:45:05 +0530

    Update mass_action.py

--- MassActionBot/plugins/mass_action.py
+++ MassActionBot/plugins/mass_action.py
@@ -1,8 +1,19 @@
-from MassActionBot import app,BOT_ID
+from MassActionBot import app,SUDOES
 from pyrogram import filters
 from MassActionBot.utils.chat_status import handle_status
+from MassActionBot.plugins.cancel_process import SPAM_CHATS
 
 @app.on_message(filters.command(["banall","unbanall"]))
 @handle_status
 async def _banUnban(_, message):
-        
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    SPAM_CHATS.append(chat_id)
+    if message.command[0] == "banall":
+        async for members in _.get_chat_members(chat_id):
+            if chat_id not in SPAM_CHATS:
+                break
+            try:
+               if user_id in SUDOES:
+                   continue 
+                
