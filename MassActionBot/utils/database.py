f16837a5a98d94a3bb2496ad65c8d9684d473ffb
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:37:02 +0530

    Update database.py

--- MassActionBot/utils/database.py
+++ MassActionBot/utils/database.py
@@ -11,10 +11,10 @@ async def is_served_chat(chat_id : int) -> bool:
     return False
 
 async def get_chats() -> list:
-    chats = chatsdb.find({"chat_id" : {"$lt" : 0}})
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
     if not chats:
         return []
     chats_list = []
     for chat in await chats.to_list(length=1000000000):
         chats_list.append(chat)
-    return chats_list    
+    return chats_list
