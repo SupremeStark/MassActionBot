c1de2ff3798c40d9eb677dda37ac99de27cb489d
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 15:36:40 +0530

    Update and rename broadcast.p1y to broadcast.py

--- MassActionBot/plugins/broadcast.py
+++ MassActionBot/plugins/broadcast.py
@@ -1,6 +1,6 @@
 from MassActionBot.utils.database import chatsdb,get_chats
 from pyrogram import filters
-from MassActionBot import app ,LOG
+from MassActionBot import app ,LOG, OWNER_ID
 
 
 @app.on_message(filters.new_chat_members, group=2)
@@ -15,4 +15,24 @@ async def addinDb(_, message: Message):
             if m.id == BOT_ID:
                 await message.reply_text(f"ᴛʜᴀɴᴋs ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ɪɴ {message.chat.title}.ғʀᴏᴍ ɴᴏᴡ ɪ ᴡɪʟʟ ᴋᴇᴇᴘ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴄʟᴇᴀɴ.")                                                   
         except Exception as er:
-            LOG.print(f"[bold red]{er})     
+            LOG.print(f"[bold red]{er})  
+
+
+   
+@app.on_message(filters.command("bcast"))
+async def broadcast_(_, message):
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴛᴇxᴛ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ɪᴛ.")
+        query = message.text.split(None,1)[1]
+    sent = 0
+    chats = []
+    schats = await get_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    print(chats)
+
+            
