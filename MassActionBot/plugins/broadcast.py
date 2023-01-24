39ee41d684bfbaee2f9b3238fe0ea7cd5b610a0a
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:26:42 +0530

    Create broadcast.py

---
+++ MassActionBot/plugins/broadcast.py
@@ -0,0 +1,8 @@
+from MassActionBot import app, OWNER_ID
+from pyrogram import filters 
+from MassActionBot.utils.database import get_chats
+
+
+@app.on_message(filters.command("bcast"))
+async def broadcast(_, message)
+    print(await get_chats())
