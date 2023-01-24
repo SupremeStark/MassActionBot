6ed8945b8c6ea6f5b085af1ea875c524302c1d74
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:27:42 +0530

    Update broadcast.py

--- MassActionBot/plugins/broadcast.py
+++ MassActionBot/plugins/broadcast.py
@@ -4,5 +4,5 @@ from MassActionBot.utils.database import get_chats
 
 
 @app.on_message(filters.command("bcast"))
-async def broadcast(_, message)
+async def broadcast(_, message):
     print(await get_chats())
