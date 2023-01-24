9c63c3b9995b1d9c0c4b2a07a423f342e33e874c
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:11:40 +0530

    Update start.py

--- MassActionBot/plugins/start.py
+++ MassActionBot/plugins/start.py
@@ -1,6 +1,11 @@
 from MassActionBot import app
-from pyrogram import filters 
+from pyrogram import filters , enums 
+from MassActionBot.utils.database import chatsdb
 
 @app.on_message(filters.command("start"))
-async def _start(_, message):    
+async def _start(_, message): 
+    chat_id = message.chat.id
+ #   print(chat_id)
+    if message.chat.type == enums.ChatType.PRIVATE:
+            
     await message.reply_text("am started")
