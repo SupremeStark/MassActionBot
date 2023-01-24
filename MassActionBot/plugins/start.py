e1cd8b6169a50dae91db43748b22167f2e0d8183
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:15:33 +0530

    Update start.py

--- MassActionBot/plugins/start.py
+++ MassActionBot/plugins/start.py
@@ -5,7 +5,8 @@ from MassActionBot.utils.database import chatsdb
 @app.on_message(filters.command("start"))
 async def _start(_, message): 
     chat_id = message.chat.id
-    print(chat_id)
-   # if message.chat.type == enums.ChatType.PRIVATE:
-            
+    print(chat_id)    
+    check = await chatsdb.find_one({"chat_id" : chat_id}) 
+    if not check:
+        await chatsdb.insert_one({"chat_id" : chat_id})       
     await message.reply_text("am started")
