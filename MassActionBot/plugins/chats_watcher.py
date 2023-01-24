e3ebf0025d7ee4970548eb1d280e813bea477a7f
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 08:53:51 +0530

    Update chats_watcher.py

--- MassActionBot/plugins/chats_watcher.py
+++ MassActionBot/plugins/chats_watcher.py
@@ -1,13 +1,13 @@
 from MassActionBot import app
 from pyrogram import filters 
-from MassActionBot.utils.database import add_served_chat
+from MassActionBot.utils.database import is_served_chat
 
 
 @app.on_message(filters.new_chat_members,group=2)
 async def addinDb(_, message):
     BOT_ID = (await _.get_me()).id
     chat_id = message.chat.id
-    h = await add_served_chat(chat_id)
+    h = await is_served_chat(chat_id)
     print(h)
     for m in message.new_chat_members:
         try:
