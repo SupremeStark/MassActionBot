8dd95e3a866ec8fe1861fe75f7a4bc9ec041e829
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 08:47:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     OWNER_ID = 5505555398
     CLONE = True
     START_PIC = "idk"
-    MONGO_DB_URL = "mongodb+srv://GOJO:liyaxlambert*143@cluster0.fhbjzax.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URL = "mongodb+srv://KAKASHI:liyaxlambert*143@cluster0.yw44qr0.mongodb.net/?retryWrites=true&w=majority"
     
