f82a5899c66ec4398113c0eb48d10efaf353be43
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 19:09:46 +0530

    Create __main__.py

---
+++ MassActionBot/__main__.py
@@ -0,0 +1,20 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+from MassActionBot.plugins import ALL_MODULES
+
+
+async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("MassActionBot.plugins." + all_module)
+    LOG.print("[bold yellow]ɴᴏᴡ ᴀᴍ ʀᴇᴀᴅʏ ᴛᴏ ғɪɢʜᴛ ʙᴏss..")
+    await idle() 
+    LOG.print("[bold red]ᴄᴀɴᴄᴇʟɪɴɢ ᴀʟʟ ᴛᴀsᴋs.")
+
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(start_bot())
+    
