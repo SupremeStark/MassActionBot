1f0e507ba6e876afad78d4c5decb0e0f9accfb9a
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 09:06:29 +0530

    Update watcher.py

--- MassActionBot/plugins/watcher.py
+++ MassActionBot/plugins/watcher.py
@@ -1,6 +1,6 @@
 from MassActionBot import app
 from pyrogram import filters 
-from MassActionBot.utils.database import is_served_chat, chatsdb
+from MassActionBot.utils.database import chatsdb
 
 
 @app.on_message(filters.new_chat_members,group=2)
@@ -16,3 +16,11 @@ async def addinDb(_, message):
                 await message.reply_text(f"ᴛʜᴀɴᴋs ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ɪɴ {message.chat.title}. ғʀᴏᴍ ɴᴏᴡ ɪ ᴡɪʟʟ ᴋᴇᴇᴘ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴄʟᴇᴀɴ.")
         except Exception as idk:
             LOG.print(f"[bold red]{idk}")
+
+
+@app.on_message(filters.private)
+async def adduserinDb(_, message):
+    chat_id = message.from_user.id
+    check = await chatsdb.find_one({"chat_id" : chat_id})
+    if not check:
+        await chatsdb.insert_one({"chat_id" : chat_id})
