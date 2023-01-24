304ebecf512a2bd61e969137d99961044e42e409
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-24 08:42:34 +0530

    Create chats_watcher.py

---
+++ MassActionBot/plugins/chats_watcher.py
@@ -0,0 +1,16 @@
+from MassActionBot import app
+from pyrogram import filters 
+from MassActionBot.utils.database import add_served_chat
+
+
+@app.on_message(filters.new_chat_members,group=2)
+async def addinDb(_, message):
+    BOT_ID = (await _.get_me()).id
+    chat_id = message.chat.id
+    await add_served_chat(chat_id)
+    async for m in message.new_chat_members:
+        try:
+            if m.id == BOT_ID:
+                await message.reply_text("ᴛʜᴀɴᴋs ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ɪɴ {}. ғʀᴏᴍ ɴᴏᴡ ɪ ᴡɪʟʟ ᴋᴇᴇᴘ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴄʟᴇᴀɴ.")
+        except Exception as idk:
+            LOG.print(f"[bold red]{idk}")
