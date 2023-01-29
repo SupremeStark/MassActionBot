6aff4d52581a6f62075c4f8cd5390808614d4616
SupremeStark <123313682+SupremeStark@users.noreply.github.com>
2023-01-29 20:03:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
-    API_ID =    # don't use "" just fill your api id here 
-    API_HASH = "" #api hash
-    BOT_TOKEN = "" #token of your bot get it from @BotFather
-    BAN_PROTECTED = [5285561060] #users who can perform any cmds and also they are ban protected
+    API_ID = 14817849   # don't use "" just fill your api id here 
+    API_HASH = "f72b972aadabef20c7ea193362f7c7ae" #api hash
+    BOT_TOKEN = "5678127761:AAFZJiC_1iUxg1LGbXK80nxSlkceMbRcEF8" #token of your bot get it from @BotFather
+    BAN_PROTECTED = [5285561060,1812344071,1109460378,764707869] #users who can perform any cmds and also they are ban protected
     OWNER_ID = 5505555398 #id of the owner note :- only owner can broadcast message 
     START_PIC = "" #not necessary 
-    MONGO_DB_URL = "" #url of your mongodb
+    MONGO_DB_URL = "mongodb+srv://sukuna:sukuna@sukuna.vipbn9a.mongodb.net/?retryWrites=true&w=majority" #url of your mongodb
     
